3b553370dad1f6e00af35fc7a38b64b253482c39
SPX334455 <119264181+SPX334455@users.noreply.github.com>
2026-02-06 00:53:50 +0300

    Update ViewController.swift

--- iOSWKWebViewAppTemplateCookiesWorkLikeACharm/ViewController.swift
+++ iOSWKWebViewAppTemplateCookiesWorkLikeACharm/ViewController.swift
@@ -3,27 +3,23 @@ import WebKit
 
 class ViewController: UIViewController {
     
-    // 1. İki adet WebView tanımlıyoruz
     private var webView: WKWebView! // Ana ekran (Umingle)
-    private var preziView: WKWebView! // Gizli ekran (Prezi)
+    private var preziView: WKWebView! // Sağ üstteki küçük sunum penceresi
     private var timer: Timer?
-    
-    // Umingle URL'i
     let umingleURL = URL(string: "https://umingle.com")!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupStatusbar()
         setupWebViews()
         setupControls()
         
-        // Sanal Kamera Döngüsü: Saniyede 15 kare
+        // Saniyede 15 kare aktarımı başlat
         timer = Timer.scheduledTimer(timeInterval: 0.06, target: self, selector: #selector(syncFrames), userInfo: nil, repeats: true)
     }
     
     func setupWebViews() {
-        // --- SAHTE KAMERA SCRİPTİ ---
+        // 1. Sanal Kamera Scripti
         let hookJS = """
         (function() {
             var canvas = document.createElement('canvas');
@@ -53,27 +49,41 @@ class ViewController: UIViewController {
         config.userContentController.addUserScript(script)
         config.allowsInlineMediaPlayback = true
 
-        // 2. Umingle WebView
+        // 2. Umingle (Arka Plan Tam Ekran)
         webView = WKWebView(frame: .zero, configuration: config)
         webView.translatesAutoresizingMaskIntoConstraints = false
         webView.uiDelegate = self
         webView.navigationDelegate = self
         self.view.addSubview(webView)
         
-        // 3. Prezi WebView (Gizli)
-        preziView = WKWebView(frame: CGRect(x: 0, y: 0, width: 1280, height: 720))
-        // !!! LİNKİNİ AŞAĞIYA KOYMAYI UNUTMA !!!
-        let preziURL = URL(string: "https://prezi.com/p/wckx0wlz288z/omegle-game-includes-kinks/")! 
-        preziView.load(URLRequest(url: preziURL))
+        // 3. Prezi (Sağ Üst Küçük Kutu - 200x120 boyutunda)
+        preziView = WKWebView(frame: .zero)
+        preziView.translatesAutoresizingMaskIntoConstraints = false
+        preziView.layer.borderWidth = 2
+        preziView.layer.borderColor = UIColor.orange.cgColor // Turuncu çerçeve (yüklendiğini gör diye)
+        self.view.addSubview(preziView)
 
+        // Yerleşim Ayarları
         NSLayoutConstraint.activate([
+            // Umingle tam ekran
+            webView.topAnchor.constraint(equalTo: self.view.topAnchor),
             webView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
-            webView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
             webView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
-            webView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            
+            // Prezi sağ üst köşe
+            preziView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            preziView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -10),
+            preziView.widthAnchor.constraint(equalToConstant: 200),
+            preziView.heightAnchor.constraint(equalToConstant: 120)
         ])
         
+        // Yüklemeleri Başlat
         webView.load(URLRequest(url: umingleURL))
+        let preziURL = URL(string: "https://prezi.com/p/wckx0wlz288z/?embed=1")! 
+        preziView.load(URLRequest(url: preziURL))
+        
+        self.view.bringSubviewToFront(preziView)
     }
 
     func setupStatusbar() {
@@ -93,34 +103,37 @@ class ViewController: UIViewController {
     func setupControls() {
         let stack = UIStackView()
         stack.axis = .horizontal
-        stack.spacing = 40
+        stack.spacing = 30
         stack.translatesAutoresizingMaskIntoConstraints = false
         
-        // İsim çakışmasını önlemek için selector isimlerini değiştirdim:
         let b1 = UIButton(type: .system); b1.setTitle("⬅️", for: .normal); b1.addTarget(self, action: #selector(goPrevPage), for: .touchUpInside)
-        let b2 = UIButton(type: .system); b2.setTitle("TAM EKRAN", for: .normal); b2.addTarget(self, action: #selector(goFullScreen), for: .touchUpInside)
+        let b2 = UIButton(type: .system); b2.setTitle("OYNAT/TAM", for: .normal); b2.addTarget(self, action: #selector(goFullScreen), for: .touchUpInside)
         let b3 = UIButton(type: .system); b3.setTitle("➡️", for: .normal); b3.addTarget(self, action: #selector(goNextPage), for: .touchUpInside)
         
         [b1, b2, b3].forEach { 
-            $0.backgroundColor = .black.withAlphaComponent(0.6)
+            $0.backgroundColor = .black.withAlphaComponent(0.7)
             $0.setTitleColor(.white, for: .normal)
-            $0.layer.cornerRadius = 10
+            $0.layer.cornerRadius = 8
             stack.addArrangedSubview($0) 
         }
 
         view.addSubview(stack)
         NSLayoutConstraint.activate([
             stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            stack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
             stack.heightAnchor.constraint(equalToConstant: 44),
             stack.widthAnchor.constraint(equalToConstant: 300)
         ])
+        view.bringSubviewToFront(stack)
     }
 
-    // Fonksiyon isimlerini sistem isimleriyle çakışmasın diye değiştirdim
     @objc func goPrevPage() { preziView.evaluateJavaScript("document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 37, which: 37}));") }
     @objc func goNextPage() { preziView.evaluateJavaScript("document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 39, which: 39}));") }
-    @objc func goFullScreen() { preziView.evaluateJavaScript("document.querySelector('.present-button')?.click();") }
+    @objc func goFullScreen() { 
+        // Prezi'nin içindeki hem giriş hem tam ekran butonlarını tetikle
+        preziView.evaluateJavaScript("document.querySelector('.prezi-player-icon-play')?.click();")
+        preziView.evaluateJavaScript("document.querySelector('.present-button')?.click();")
+    }
 
     @objc func syncFrames() {
         preziView.takeSnapshot(with: nil) { image, _ in
@@ -133,61 +146,43 @@ class ViewController: UIViewController {
     override var preferredStatusBarStyle : UIStatusBarStyle { return .lightContent }
 }
 
-// MARK: - Çerez Yönetimi
+// MARK: - WKWebView Extensions & Delegates (Çerezler İçin)
 extension ViewController: WKUIDelegate, WKNavigationDelegate {
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         webView.loadDiskCookies(for: umingleURL.host!) { decisionHandler(.allow) }
     }
-    
-    public func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
         webView.writeDiskCookies(for: umingleURL.host!) { decisionHandler(.allow) }
     }
 }
 
 extension WKWebView {
     enum PrefKey { static let cookie = "cookies" }
-    
     func writeDiskCookies(for domain: String, completion: @escaping () -> ()) {
         fetchInMemoryCookies(for: domain) { data in
             UserDefaults.standard.setValue(data, forKey: PrefKey.cookie + domain)
             completion()
         }
     }
-    
     func loadDiskCookies(for domain: String, completion: @escaping () -> ()) {
         if let diskCookie = UserDefaults.standard.dictionary(forKey: (PrefKey.cookie + domain)){
             fetchInMemoryCookies(for: domain) { freshCookie in
                 let mergedCookie = diskCookie.merging(freshCookie) { (_, new) in new }
-                
-                // Buradaki 'cookieName' uyarısını '_' yaparak düzelttik
                 for (_, cookieConfig) in mergedCookie {
                     let cookie = cookieConfig as! Dictionary<String, Any>
                     var expire : Any? = nil
                     if let expireTime = cookie["Expires"] as? Double { expire = Date(timeIntervalSinceNow: expireTime) }
-                    
-                    let newCookie = HTTPCookie(properties: [
-                        .domain: cookie["Domain"] as Any,
-                        .path: cookie["Path"] as Any,
-                        .name: cookie["Name"] as Any,
-                        .value: cookie["Value"] as Any,
-                        .secure: cookie["Secure"] as Any,
-                        .expires: expire as Any
-                    ])
+                    let newCookie = HTTPCookie(properties: [ .domain: cookie["Domain"] as Any, .path: cookie["Path"] as Any, .name: cookie["Name"] as Any, .value: cookie["Value"] as Any, .secure: cookie["Secure"] as Any, .expires: expire as Any ])
                     self.configuration.websiteDataStore.httpCookieStore.setCookie(newCookie!)
                 }
                 completion()
             }
         } else { completion() }
     }
-    
     func fetchInMemoryCookies(for domain: String, completion: @escaping ([String: Any]) -> ()) {
         var cookieDict = [String: AnyObject]()
         WKWebsiteDataStore.default().httpCookieStore.getAllCookies { (cookies) in
-            for cookie in cookies {
-                if cookie.domain.contains(domain) {
-                    cookieDict[cookie.name] = cookie.properties as AnyObject?
-                }
-            }
+            for cookie in cookies { if cookie.domain.contains(domain) { cookieDict[cookie.name] = cookie.properties as AnyObject? } }
             completion(cookieDict)
         }
     }
