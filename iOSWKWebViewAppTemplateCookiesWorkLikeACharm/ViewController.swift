68449b39aaa922968e5eccb3f13095cc23034101
SPX334455 <119264181+SPX334455@users.noreply.github.com>
2026-02-06 15:17:07 +0300

    Update ViewController.swift

--- iOSWKWebViewAppTemplateCookiesWorkLikeACharm/ViewController.swift
+++ iOSWKWebViewAppTemplateCookiesWorkLikeACharm/ViewController.swift
@@ -9,19 +9,26 @@ class ViewController: UIViewController {
     private var timer: Timer?
     let umingleURL = URL(string: "https://umingle.com")!
 
-    // 📱 EKRANI YATAY TUTMAK İÇİN
+    // 1. EKRANI YATAYDA SABİTLE (iOS 16+ VE ÖNCESİ İÇİN)
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask { .landscape }
     override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation { .landscapeLeft }
 
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        if #available(iOS 16.0, *) {
+            let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+            windowScene?.requestGeometryUpdate(.iOS(interfaceOrientations: .landscape))
+        } else {
+            UIDevice.current.setValue(UIInterfaceOrientation.landscapeLeft.rawValue, forKey: "orientation")
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         requestNativePermissions()
         setupStatusbar()
         setupWebViews()
         setupControls()
-        
-        // Aktarımı başlat
         timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(syncFrames), userInfo: nil, repeats: true)
     }
     
@@ -31,54 +38,50 @@ class ViewController: UIViewController {
     }
     
     func setupWebViews() {
+        // --- SAHTE KAMERA SCRİPTİ ---
         let hookJS = """
         (function() {
             var canvas = document.createElement('canvas');
             canvas.width = 1280; canvas.height = 720;
             var ctx = canvas.getContext('2d');
             
-            // Başlangıçta siteye "Yükleniyor" görüntüsü verelim (Siyah kalmasın)
+            // İlk açılışta siyah ekran yerine mavi bir ekran verelim (Çalıştığını anlamak için)
             ctx.fillStyle = "blue";
             ctx.fillRect(0,0,1280,720);
             ctx.fillStyle = "white";
-            ctx.font = "40px Arial";
-            ctx.fillText("Sanal Kamera Aktif - Prezi Bekleniyor...", 100, 360);
+            ctx.font = "30px Arial";
+            ctx.fillText("Sanal Kamera Aktif - Prezi Bekleniyor", 50, 360);
 
             window.drawToFakeCamera = function(b64) {
                 var img = new Image();
                 img.onload = function() { 
-                    ctx.clearRect(0, 0, 1280, 720);
+                    ctx.clearRect(0,0,1280,720);
                     ctx.drawImage(img, 0, 0, 1280, 720); 
                 };
                 img.src = 'data:image/jpeg;base64,' + b64;
             };
 
-            // 🟢 KRİTİK: Cihazları siteye zorla kabul ettir
             navigator.mediaDevices.enumerateDevices = function() {
                 return Promise.resolve([
-                    {deviceId:'virt-cam', kind:'videoinput', label:'FaceTime HD Camera', groupId:'g1'},
-                    {deviceId:'virt-mic', kind:'audioinput', label:'Built-in Microphone', groupId:'g2'}
+                    {deviceId:'v-cam', kind:'videoinput', label:'FaceTime HD Camera (Built-in)', groupId:'g1'},
+                    {deviceId:'v-mic', kind:'audioinput', label:'Internal Microphone', groupId:'g2'}
                 ]);
             };
 
-            function getFakeStream() {
+            function getStream() {
                 var stream = canvas.captureStream(30);
-                var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-                var dst = audioCtx.createMediaStreamDestination();
+                var ac = new (window.AudioContext || window.webkitAudioContext)();
+                var dst = ac.createMediaStreamDestination();
                 stream.addTrack(dst.stream.getAudioTracks()[0] || new MediaStreamTrack());
                 return stream;
             }
 
-            // Site kamera istediğinde akışı bağla
-            navigator.mediaDevices.getUserMedia = function(c) { return Promise.resolve(getFakeStream()); };
+            navigator.mediaDevices.getUserMedia = function(c) { return Promise.resolve(getStream()); };
             
-            // WebRTC bağlantılarını yakala
-            var origAddTrack = RTCPeerConnection.prototype.addTrack;
-            RTCPeerConnection.prototype.addTrack = function(track, stream) {
-                if (track.kind === 'video') {
-                    return origAddTrack.call(this, canvas.captureStream(30).getVideoTracks()[0], stream);
-                }
-                return origAddTrack.call(this, track, stream);
+            var orig = RTCPeerConnection.prototype.addTrack;
+            RTCPeerConnection.prototype.addTrack = function(t, s) {
+                if (t.kind === 'video') { return orig.call(this, canvas.captureStream(30).getVideoTracks()[0], s); }
+                return orig.call(this, t, s);
             };
         })();
         """
@@ -88,35 +91,35 @@ class ViewController: UIViewController {
         config.userContentController.addUserScript(script)
         config.allowsInlineMediaPlayback = true
         
+        // 🖥️ KENDİNİ BİLGİSAYAR OLARAK TANIT (SİTEYİ KANDIRMAK İÇİN)
         webView = WKWebView(frame: .zero, configuration: config)
         webView.translatesAutoresizingMaskIntoConstraints = false
         webView.uiDelegate = self
         webView.navigationDelegate = self
-        // Masaüstü Chrome gibi davran (En stabil mod)
-        webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+        webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
         self.view.addSubview(webView)
         
         preziView = WKWebView(frame: .zero)
         preziView.translatesAutoresizingMaskIntoConstraints = false
-        preziView.layer.borderWidth = 3
+        preziView.layer.borderWidth = 2
         preziView.layer.borderColor = UIColor.green.cgColor
         self.view.addSubview(preziView)
 
         NSLayoutConstraint.activate([
-            webView.topAnchor.constraint(equalTo: self.view.topAnchor),
-            webView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
-            webView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
-            webView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            webView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             
-            preziView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            preziView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -10),
+            preziView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            preziView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
             preziView.widthAnchor.constraint(equalToConstant: 240),
             preziView.heightAnchor.constraint(equalToConstant: 135)
         ])
         
         webView.load(URLRequest(url: umingleURL))
         preziView.load(URLRequest(url: URL(string: "https://prezi.com/p/wckx0wlz288z/?embed=1")!))
-        self.view.bringSubviewToFront(preziView)
+        view.bringSubviewToFront(preziView)
     }
 
     func setupStatusbar() {
@@ -134,11 +137,9 @@ class ViewController: UIViewController {
     func setupControls() {
         let stack = UIStackView()
         stack.axis = .horizontal; stack.spacing = 20; stack.translatesAutoresizingMaskIntoConstraints = false
-        
-        let b1 = UIButton(type: .system); b1.setTitle(" ⬅️ ", for: .normal); b1.addTarget(self, action: #selector(goPrev), for: .touchUpInside)
-        let b2 = UIButton(type: .system); b2.setTitle(" BAŞLAT ", for: .normal); b2.addTarget(self, action: #selector(forcePlay), for: .touchUpInside)
-        let b3 = UIButton(type: .system); b3.setTitle(" ➡️ ", for: .normal); b3.addTarget(self, action: #selector(goNext), for: .touchUpInside)
-        
+        let b1 = UIButton(type: .system); b1.setTitle(" ⬅️ ", for: .normal); b1.addTarget(self, action: #selector(goP), for: .touchUpInside)
+        let b2 = UIButton(type: .system); b2.setTitle(" BAŞLAT ", for: .normal); b2.addTarget(self, action: #selector(fP), for: .touchUpInside)
+        let b3 = UIButton(type: .system); b3.setTitle(" ➡️ ", for: .normal); b3.addTarget(self, action: #selector(goN), for: .touchUpInside)
         [b1, b2, b3].forEach {
             $0.backgroundColor = .black; $0.setTitleColor(.white, for: .normal); $0.layer.cornerRadius = 10
             stack.addArrangedSubview($0)
@@ -152,9 +153,9 @@ class ViewController: UIViewController {
         view.bringSubviewToFront(stack)
     }
 
-    @objc func goPrev() { preziView.evaluateJavaScript("window.focus(); document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 37, bubbles: true}));") }
-    @objc func goNext() { preziView.evaluateJavaScript("window.focus(); document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 39, bubbles: true}));") }
-    @objc func forcePlay() {
+    @objc func goP() { preziView.evaluateJavaScript("window.focus(); document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 37, bubbles: true}));") }
+    @objc func goN() { preziView.evaluateJavaScript("window.focus(); document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 39, bubbles: true}));") }
+    @objc func fP() {
         preziView.evaluateJavaScript("document.querySelector('.prezi-player-icon-play')?.click(); document.querySelector('.present-button')?.click();")
     }
 
@@ -167,17 +168,15 @@ class ViewController: UIViewController {
     }
 }
 
-// MARK: - İzinler ve Çerezler
+// Delegate ve Cookie fonksiyonları (Değişmedi)
 extension ViewController: WKUIDelegate, WKNavigationDelegate {
     @available(iOS 15.0, *)
     func webView(_ webView: WKWebView, requestMediaCapturePermissionFor origin: WKSecurityOrigin, initiatedByFrame frame: WKFrameInfo, type: WKMediaCaptureType, decisionHandler: @escaping (WKPermissionDecision) -> Void) {
         decisionHandler(.grant)
     }
-
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         webView.loadDiskCookies(for: umingleURL.host!) { decisionHandler(.allow) }
     }
-
     func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
         webView.writeDiskCookies(for: umingleURL.host!) { decisionHandler(.allow) }
     }
